79bda3bb176f1447074ec812a18fef58c1fe6749
papu <yuki0606papkon9690@icloud.com>
2024-02-15 09:17:33 +0900

    [Add] index

--- .py
+++ .py
@@ -1,9 +1,11 @@
 import openpyxl
+import csv
 
 # パスの定義
 static_path = "static"
 search_method_excel_path = static_path + "/input_excel/search_method.xlsx"
 output_reins_excel_path = static_path + "/output_excel/output_reins.xlsx"
+output_reins_csv_path = static_path + "/csv/output_reins.csv"
 
 
 def excel_to_list(input_excel_path: str = "input.xlsx"):
@@ -30,11 +32,34 @@ def list_to_excel(to_excel_list: list , output_excel_path: str = "output.xlsx"):
             sheet.cell(row=row+1, column=col+1).value = to_excel_list[row][col]
     workbook.save(output_excel_path)
     
+def csv_to_list(csv_path: str = "output.csv"):
+    """ 多次元データを含むcsvからリストに変換 """
+    data_list = []
+    with open(csv_path, 'r' , encoding="utf-8-sig") as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            data_list.append(row)
+    return data_list
+
+
+def csv_to_excel(input_csv_path, output_excel_path):
+    """ csvファイルをExcelファイルに変換する関数 """
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    # CSVファイルを開き、行ごとにExcelシートに書き込む
+    with open(input_csv_path, 'r', newline='', encoding='utf-8') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row_index, row in enumerate(csv_reader, start=1):
+            for col_index, value in enumerate(row, start=1):
+                sheet.cell(row=row_index, column=col_index, value=value)
+    # Excelファイルに保存
+    workbook.save(output_excel_path)
+
 
 def main():
-    data_list = excel_to_list(search_method_excel_path)
-    print(data_list)
-    list_to_excel(data_list , output_reins_excel_path)
+    csv_to_excel(output_reins_csv_path , output_reins_excel_path)
+    excel_list = excel_to_list(output_reins_excel_path)
+    list_to_excel(excel_list , output_reins_excel_path)
 
 
 if __name__ == "__main__":
